f8f24a8af71c0887750bb905c8e8407f74348170
Priyanshu Goyal <priyanshugoyal2005@gmail.com>
2025-10-05 15:02:34 +0530

    Add files via upload

---
+++ Assignment_7/leet_198.java
@@ -0,0 +1,22 @@
+public class leet_198 {
+    public static void main(String[] args) {
+        int[] nums = { 2,7,9,3,1 };
+        System.out.println(rob(nums));    
+    }
+
+    public static int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i=2; i<=nums.length; i++){
+            int skipped = dp[i-1];
+            int robbed = nums[i-1] + dp[i-2];
+            dp[i] = Math.max(skipped, robbed);
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<dp.length; i++){
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
